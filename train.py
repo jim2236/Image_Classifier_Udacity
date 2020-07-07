7ea8e7e925f8f3627a14e0e052ad20c2adf167ce
Jim Michael <jim.o.michael@gmail.com>
2020-07-07 19:51:34 -0400

    20200707

--- train.py
+++ train.py
@@ -2,7 +2,7 @@
 # Train a network and display accuracy results
 # Jim Michael 7/4/20
 
-# Import software modules
+# Import software modules 
 #%matplotlib inline
 #%config InlineBackend.figure_format = 'retina'
 
@@ -46,9 +46,12 @@ args = parser.parse_args()
 if args.data_dir != 'flowers':
     os.system('mkdir '+ args.data_dir)
     print(args.data_dir)
-    os.chdir(str(args.data_dir))    # <<<<<<<<< problem here
+    os.chdir(str(args.data_dir))    # <<<<<<<<< problem here 
     #zz = os.getcwd()
     #print(zz)
+    os.chdir('/home/workspace/ImageClassifier')
+    zzz = os.getcwd()
+    print(zzz)
     os.system('mkdir train')
     os.system('mkdir valid')
     os.system('mkdir test')
@@ -64,8 +67,8 @@ else:
     test_dir = data_dir + '/test'
 
 
-# Create data transforms
-
+# Create data transforms 
+print("Starting Transforms")
 train_transforms = transforms.Compose([transforms.RandomRotation(30),
                                       transforms.RandomResizedCrop(224),
                                       transforms.RandomHorizontalFlip(),
@@ -94,9 +97,9 @@ validation_data = datasets.ImageFolder(valid_dir, transform = validation_transfo
 test_data = datasets.ImageFolder(test_dir, transform = test_transforms)
 
 # TODO: Using the image datasets and the trainforms, define the dataloaders
-#dataloaders =
+#dataloaders = 
 
-train_loader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True)
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True) 
 
 validation_loader = torch.utils.data.DataLoader(validation_data, batch_size=64, shuffle=False)
 
@@ -105,7 +108,7 @@ test_loader = torch.utils.data.DataLoader(test_data, batch_size=64, shuffle=Fals
 
 with open('cat_to_name.json', 'r') as f:
     cat_to_name = json.load(f)
-
+    
 
 # Move to GPU is available
 
@@ -114,20 +117,12 @@ if args.gpu == True:
 else:
     device = torch.device("cpu")
 
-
 #device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-
-# Build the network
-
-#arch = args.arch
-#model_cmd = 'models.'  + arch + '(pretrained=True)'
-#model = model_cmd
 
 model = getattr(models, args.arch)(pretrained=True)
 
 #model = models.vgg16(pretrained=True)
-
 
 
 # Freeze parametes to avoid backprop
@@ -135,12 +130,7 @@ for param in model.parameters():
     param.requires_grad=False
 
 
-hidden_units = args.hidden_units
-hu1 = hidden_units[0]
-hu2 = hidden_units[1]
-
-
-# Define the model
+# Define the model   
 model.classifier = nn.Sequential(OrderedDict([
                             ('fc1', nn.Linear(25088, hu1)),
                             ('relu1', nn.ReLU()),
@@ -165,58 +155,65 @@ model.to(device)
 
 
 # Train the network
+print("Train the network")  # remove
 
-epochs = args.epochs
+epochs = args.epochs 
 
 steps = 0
+steps_2 = 0   # remove
+steps_3 = 0.  # remove
 running_loss = 0
 print_every = 5
 
 for epoch in range(epochs):
     for inputs, labels in train_loader:
-        steps += 1
-
+        steps += 1 
+        print("Step ", steps)  # remove       
         inputs, labels = inputs.to(device), labels.to(device)
 
         optimizer.zero_grad()
-
+        
         logps = model.forward(inputs)
         loss = criterion(logps, labels)  # what about flower_to_name?
         loss.backward()
         optimizer.step()
-
+        
         # Validation
         running_loss += loss.item()
-
+        
         if steps % print_every == 0:
+            steps_2 += 1  # remove
+            print(steps_2)  # remove 
             validation_loss = 0
             accuracy = 0
             model.eval()
             with torch.no_grad():
                 for inputs, labels in validation_loader:
-
+                    steps_3 += 1   # remove 
+                    print(steps_3)
+                    
                     inputs, labels = inputs.to(device), labels.to(device)
                     logps = model.forward(inputs)
                     batch_loss = criterion(logps, labels)
-
+                
                     validation_loss += batch_loss.item()
-
-                    # Calculate accuracy
+                
+                    # Calculate accuracy 
                     ps = torch.exp(logps)
                     top_p, top_class = ps.topk(1, dim=1)
                     equals = top_class == labels.view(*top_class.shape)
                     accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
-
+                     
             print(f"Epoch {epoch+1} / {epochs}.."
                   f"Train Loss:  {running_loss / print_every: .3f}.."
                   f"Validation Loss: {validation_loss/len(validation_loader): .3f}.. "
                   f"Validation Accuracy: {accuracy / len(validation_loader): .3f}")
             running_loss = 0
             model.train()
-
-
+          
+            
 # Run validation on the test set
-
+print("Run validation on test set")
 test_loss = 0
 accuracy = 0
 model.eval()
@@ -225,21 +222,21 @@ with torch.no_grad():
         inputs, labels = inputs.to(device), labels.to(device)
         logps = model.forward(inputs)
         batch_loss = criterion(logps, labels)
-
+    
         test_loss += batch_loss.item()
-
+    
         # Calculate accuracy
         ps = torch.exp(logps)
         top_p, top_class = ps.topk(1, dim=1)
         equals = top_class == labels.view(*top_class.shape)
         accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
-
-    print(f"Test Accuracy: {accuracy/len(test_loader): .3f}")
     
-model.train()
+    print(f"Test Accuracy: {accuracy/len(test_loader): .3f}") 
+    
+model.train()    
 
 
-# Save the checkpoint
+# Save the checkpoint 
 
 model.class_to_idx = train_data.class_to_idx
 
@@ -247,9 +244,9 @@ checkpoint = {'input size': 25088,
               'hidden layers': [512, 256],
               'output size': 102,
               'state_dict': model.state_dict(),
-              'class_to_idx': model.class_to_idx}
+              'class_to_idx': model.class_to_idx}  
 
 torch.save(checkpoint,'checkpoint.pth')
 
-print("Done")
+print("Done")  # remove 
 # End
